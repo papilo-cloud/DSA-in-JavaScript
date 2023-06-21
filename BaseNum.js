f7da6ea2c3ae9be83e454b43b6fba5ee4723aad3
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2023-06-21 03:31:27 -0700

    Implementing base conversion with stack

---
+++ BaseNum.js
@@ -0,0 +1,21 @@
+// Multiple Base Conversions
+
+class Base{
+    constructor(){
+       this.data = [];
+    }
+    baseFunction(number, base){
+       let ans = '';
+       do {
+          this.data.push(number%base)
+          number = Math.floor(number / base)
+       } while (number > 0);
+       while (this.data.length > 0) {
+          ans += this.data.pop();
+       }
+       return ans;
+   }
+ }
+ 
+ const base = new Base();
+ console.log(base.baseFunction(23, 2));
