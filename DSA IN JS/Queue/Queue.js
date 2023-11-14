22d5b64dd4e066418ad7d051628c591b09fc1c01
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2023-11-14 14:13:09 -0800

    queue

--- DSA IN JS/Queue/Queue.js
+++ DSA IN JS/Queue/Queue.js
@@ -1,5 +1,24 @@
 class Queue {
     constructor() {
         this.dataStore = [];
+        this.top = 0;
+    }
+    enqueue(element){
+        this.dataStore[this.top++] = element;
+    }
+    dequeue() {
+        return this.dataStore.shift();
+    }
+    isEmpty(){
+        if (this.dataStore.length == 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+    peek(){
+            return this.dataStore[this.dataStore.length-1];
     }
 }
+
+const queue = new Queue();
