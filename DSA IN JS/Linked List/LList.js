1d47584fe9a3bcfcbcae8485b5272e5402e61461
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2023-11-24 02:02:59 -0800

    linked list

---
+++ DSA IN JS/Linked List/LList.js
@@ -0,0 +1,14 @@
+/*
+Connected data that is dispersed throughout memory are known as nodes.
+In a linked list, each node represents one item in the list. The big question,
+then, is: if the nodes are not next to each other in memory, how does the
+computer know which nodes are part of the same linked list?
+This is the key to the linked list: each node also comes with a little extra
+information, namely, the memory address of the next node in the list.
+This extra piece of data—this pointer to the next node’s memory address—is
+known as a link. 
+
+ */
+
+// (A linked list’s first node can also be referred to as its head, and its final node
+// as its tail.)
