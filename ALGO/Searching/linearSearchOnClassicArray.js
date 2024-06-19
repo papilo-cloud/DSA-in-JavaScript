d78182d28892b0f215861429d590b3c1d85bc5c4
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-06-19 10:33:45 +0100

    Linear search on a Classic Array

--- ALGO/Searching/linearSearchOnClassicArray.js
+++ ALGO/Searching/linearSearchOnClassicArray.js
@@ -1,12 +1,16 @@
 // Lineae search on a Classic Array
 
 function linearSearch(array, searchValue) {
+    let count = 0
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
+        count++
         if (element === searchValue) {
-            return index
+            console.log(`number of steps: ${count}`)
+            return `found at index: ${index}`
         }
     }
+    console.log(`number of steps: ${count}`)
     return 'no match'
 }
 
